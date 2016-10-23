ea253653eb793fe89c65beacd98344e3fa35d548
samlishen <shenl95@uw.edu>
2016-10-23 09:57:18 -0700

    Init

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using HoloToolkit.Unity;
+
+public class Database : SpatialMappingSource {
+    /// <summary>
+    /// Type of devices that this software can run on.
+    /// </summary>
+    public enum DeviceType
+    {
+        MasterClient,
+        HoloLens,
+        Vive,
+        Kinect,
+        Desktop
+    }
+
+    public static DeviceType Type;
+    private static byte[] _mesh;
+    private static int _currentMeshVersion = 0;
+    private static int _lastestMeshVersion = 0;
+
+    public SpatialMappingManager SpatialMappingManager;
+    public static SpatialMappingManager MappingManager;
+
+    void Awake()
+    {
+        base.Awake();
+        MappingManager = SpatialMappingManager;
+        DetactDeviceType();
+    }
+
+    void Update()
+    {
+        if (Type == DeviceType.MasterClient)
+        {
+            if (Input.GetKeyDown("l"))
+            {
+                DisplayMeshes();
+            }
+        }
+        else if (Type == DeviceType.HoloLens)
+        {
+            
+        }
+        else
+        {
+            if (_currentMeshVersion != _lastestMeshVersion)
+            {
+                DisplayMeshes();
+                _currentMeshVersion = _lastestMeshVersion;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This method will detect which <see cref="DeviceType"/> is the software currently running on. 
+    /// <see cref="DeviceType.Kinect"/> is currently not working. It will be detacted as desktop.
+    /// </summary>
+    private static void DetactDeviceType()
+    {
+#if (UNITY_STANDALONE_WIN && !UNITY_EDITOR) && !UNITY_WSA_10_0
+        List<string> supportedDeviceList = new List<string>(VRSettings.supportedDevices);
+        if (supportedDeviceList.Contains("OpenVR")) {
+            Type = DeviceType.Vive;
+        }
+        return;
+#elif !UNITY_EDITOR && UNITY_WSA_10_0
+        List<string> supportedDeviceList = new List<string>(VRSettings.supportedDevices);
+        if (supportedDeviceList.Contains("HoloLens")) {
+            Type = DeviceType.HoloLens;
+        }
+#else
+        Type = DeviceType.Desktop;
+#endif
+    }
+
+    public static byte[] GetMeshAsBytes()
+    {
+        return _mesh;
+    }
+
+    public static IEnumerable<Mesh> GetMeshAsEnumerable()
+    {
+        return SimpleMeshSerializer.Deserialize(_mesh);
+    }
+
+    public static void UpdateMesh(byte[] newMesh)
+    {
+        _mesh = newMesh;
+        _lastestMeshVersion++;
+    }
+
+    public static void UpdateMesh(IEnumerable<Mesh> newMesh)
+    {
+        _mesh = SimpleMeshSerializer.Serialize(newMesh);
+        _lastestMeshVersion++;
+    }
+
+    public static void LoadMesh()
+    {
+        // Master client will load mesh from disk
+        if (Type == DeviceType.MasterClient)
+        {
+            _mesh = SimpleMeshSerializer.Serialize(MeshSaver.Load("RoomMesh"));
+        }
+        // HoloLens will scan the room and creates mesh
+        else if (Type == DeviceType.HoloLens)
+        {
+            // The following code is copying from UWB-ARSandbox/Assets/Scripts/netWorkManager.cs:sendMeshToUnity()
+            List<MeshFilter> meshFilters = MappingManager.GetMeshFilters();
+            List<Mesh> meshes = new List<Mesh>();
+
+            foreach (var meshFilter in meshFilters)
+            {
+                Mesh mesh = meshFilter.sharedMesh;
+                Mesh clone = new Mesh();
+                List<Vector3> verts = new List<Vector3>();
+                verts.AddRange(mesh.vertices);
+
+                for (int i = 0; i < verts.Count; i++)
+                {
+                    verts[i] = meshFilter.transform.TransformPoint(verts[i]);
+                }
+
+                clone.SetVertices(verts);
+                clone.SetTriangles(mesh.triangles, 0);
+                meshes.Add(clone);
+            }
+
+            _mesh = SimpleMeshSerializer.Serialize(meshes);
+        }
+    }
+
+    public static void SaveMesh()
+    {
+        if (Type == DeviceType.MasterClient)
+        {
+            MeshSaver.Save("RoomMesh", SimpleMeshSerializer.Deserialize(_mesh));
+        }
+    }
+
+    public void DisplayMeshes()
+    {
+        var meshes = SimpleMeshSerializer.Deserialize(_mesh);
+        Debug.Log(meshes.Count());
+        foreach (var mesh in meshes) {
+            GameObject surface = AddSurfaceObject(mesh, string.Format("Beamed-{0}", SurfaceObjects.Count), transform);
+            surface.transform.parent = SpatialMappingManager.Instance.transform;
+            surface.GetComponent<MeshRenderer>().enabled = true;
+            surface.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using WebSocketSharp;
+
+public class Launcher : Photon.PunBehaviour
+{
+    [Tooltip("Max player that can be in a room")] private static byte MaxPlayer = 4;
+    [Tooltip("Game version number")] private static string Version = "1";
+    [Tooltip("Port number that server sits on")] public int Port = 25827;
+
+    void Awake() {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+
+        PhotonNetwork.autoJoinLobby = false;
+
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+	    Connect();
+        Debug.Log(Database.Type);
+	}
+
+    public override void OnPhotonJoinRoomFailed(object[] codeAndMsg)
+    {
+        if (Database.Type == Database.DeviceType.Desktop) {
+            Debug.Log("Creates new room and becomes the master client");
+            Database.Type = Database.DeviceType.MasterClient;
+            PhotonNetwork.CreateRoom("UWBMixedReality", new RoomOptions() { MaxPlayers = MaxPlayer }, null);
+        } else {
+            Debug.Log("Server not found. Disconnect from PUN");
+            PhotonNetwork.Disconnect();
+        }
+    }
+
+    /// <summary>
+    /// Setting up a TCP server and listen on port sepecify earlier in the code. Once a connection comes in, start a new thread that send mesh to the connected client.
+    /// </summary>
+    public override void OnCreatedRoom()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, Port);
+        server.Start();
+        new Thread(() =>
+            {
+                TcpClient client = server.AcceptTcpClient();
+                new Thread(() =>
+                {
+                    var steam = client.GetStream();
+                    steam.Write(Database.GetMeshAsBytes(), 0, Database.GetMeshAsBytes().Length);
+                }).Start();
+            }
+        ).Start();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRoom("UWBMixedReality");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (Database.Type == Database.DeviceType.MasterClient)
+        {
+            Database.LoadMesh();
+        }
+        // Scan and send mesh to master client if you are HoloLens
+        else if (Database.Type == Database.DeviceType.HoloLens)
+        {
+            Database.LoadMesh();
+
+            TcpListener server = new TcpListener(IPAddress.Any, Port + 1);
+            server.Start();
+            new Thread(() =>
+            {
+                var client = server.AcceptTcpClient();
+                var steam = client.GetStream();
+                steam.Write(Database.GetMeshAsBytes(), 0, Database.GetMeshAsBytes().Length);
+                client.Close();
+            }).Start();
+            
+            photonView.RPC("UpdateMesh", PhotonTargets.MasterClient, GetLocalIpAddress() + ":" + (Port + 1));
+        }
+        // Connect to master client and receive meshes
+        else
+        {
+            photonView.RPC("SendMesh", PhotonTargets.MasterClient, photonView.owner.ID);
+        }
+    }
+
+    [PunRPC]
+    public void SendMesh(int id)
+    {
+        photonView.RPC("UpdateMesh", PhotonPlayer.Find(id), GetLocalIpAddress() + ":" + Port);
+    }
+
+    /// <summary>
+    /// Update mesh in database
+    /// </summary>
+    [PunRPC]
+    public void UpdateMesh(string networkConfig)
+    {
+        // Master client will update its own mesh and push them to all client
+        if (Database.Type == Database.DeviceType.MasterClient)
+        {
+            ReceiveMeshFromNetworkConfig(networkConfig);
+            photonView.RPC("UpdateMesh", PhotonTargets.Others, GetLocalIpAddress() + ":" + Port);
+        }
+        // HoloLens client will do nothing
+        else if (Database.Type == Database.DeviceType.HoloLens)
+        {
+            
+        }
+        // Otherwise, connect to server and receive mesh
+        else
+        {
+            ReceiveMeshFromNetworkConfig(networkConfig);
+        }
+    }
+
+    /// <summary>
+    /// This method will connect this client to PUN Server
+    /// </summary>
+    private void Connect()
+    {
+        if (PhotonNetwork.connected)
+        {
+            PhotonNetwork.JoinRoom("UWBMixedReality");
+        } 
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings(Version);
+        }
+    }
+
+    /// <summary>
+    /// Get local IP address
+    /// </summary>
+    /// <returns>Local IP address</returns>
+    private IPAddress GetLocalIpAddress() {
+        IPAddress retVal = null;
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList) {
+            if (ip.AddressFamily.ToString() == "InterNetwork") {
+                retVal = ip;
+            }
+        }
+        return retVal;
+    }
+
+    private void ReceiveMeshFromNetworkConfig(string networkConfig)
+    {
+        var serverIpConfig = networkConfig.Split(':');
+        IPAddress ipAddress = IPAddress.Parse(serverIpConfig[0]);
+        int port = int.Parse(serverIpConfig[1]);
+
+        TcpClient client = new TcpClient();
+        client.Connect(ipAddress, port);
+
+        using (var networkStream = client.GetStream()) {
+            byte[] data = new byte[1024];
+
+            Debug.Log("Start receiving mesh");
+            using (MemoryStream ms = new MemoryStream()) {
+                int numBytesRead;
+                while ((numBytesRead = networkStream.Read(data, 0, data.Length)) > 0) {
+                    ms.Write(data, 0, numBytesRead);
+                }
+                Debug.Log("finish receiving mesh: size = " + ms.Length);
+                client.Close();
+                Database.UpdateMesh(ms.ToArray());
+            }
+        }
+    }
+}
